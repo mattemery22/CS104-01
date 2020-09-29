4b74c5100f83bf1f5a807b89e997bdaa1d938bea
mattemery22 <71726079+mattemery22@users.noreply.github.com>
2020-09-29 14:32:38 -0400

    This is second commit

--- PythonDay1.py
+++ PythonDay1.py
@@ -1,9 +1,19 @@
-numOfTest=int(input("enter"))
-score1=float(input("enter score 1"))
-score2=float(input("enter score 2"))
-scoreTotal=score1+score2
-avg=scoreTotal/2
-print(avg)
+##numOfTest=int(input("Enter number of scores:"))
+##score1=float(input("enter score 1:"))
+##score2=float(input("enter score 2:"))
+##scoreTotal=score1+score2
+##avg=scoreTotal/2
+##print(avg)
 
 
-
+numOfTestScores=int(input("Enter number of scores:"))
+x=0
+scoreTotal=0
+while x<numOfTestScores:
+    score=float(input("Enter a score:"))
+    scoreTotal+=score
+    x+=1
+    
+average=scoreTotal/numOfTestScores
+totalSentence= "The average of the {} scores entered is {:.2f}%."
+print(totalSentence.format(numOfTestScores,average))
